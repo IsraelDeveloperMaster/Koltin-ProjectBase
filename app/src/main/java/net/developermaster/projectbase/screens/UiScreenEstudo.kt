8d872836e899794fb08e2e32b8af6054fe4aa3ee
Israel Santos <dev@developermaster.net>
2025-02-19 20:24:12 +0100

    Update: Update Dependencies, Add Intro Screen, and Improve UI
    This commit updates dependencies, adds a new intro screen with enhanced UI elements, and introduces Android Studio Preview support.
    
    -   **Dependencies:** Updated `agp` to version 8.8.1 and added `ui-tooling` and `ui-tooling-preview`.
    -   **build.gradle.kts:** Added implementation for `ui-tooling` and `ui-tooling-preview`.
    -   **UiScreenEstudo:** Created a new screen `UiScreenEstudo.kt` with an intro layout, including:
        -   Intro logo.
        -   Title and subtitle texts.
        -   A button with "Let's Get Started" text.
        -   A text for "Already have an account?".
    -   **strings.xml:** Added new strings for intro screen content.
    - **ProjectBase Sem KMP.iml:** Add new project iml
    - **Project_Default.xml:** Add new inspectionProfile
    - **Project-Base.app.iml**: Add module classLoaderOverlays

--- app/src/main/java/net/developermaster/projectbase/screens/UiScreenEstudo.kt
+++ app/src/main/java/net/developermaster/projectbase/screens/UiScreenEstudo.kt
@@ -3,24 +3,38 @@ package net.developermaster.projectbase.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.developermaster.projectbase.R
 
 @Composable
-fun UiScreenEstudo() {
+fun UiScreenEstudo(onClick: () -> Unit = {}) {
     println("UiScreenEstudo1")
 
     Column(
         modifier = Modifier
+            .padding(16.dp)
             .background(Color.White)
             .fillMaxSize()
             .verticalScroll(rememberScrollState()),
@@ -32,9 +46,65 @@ fun UiScreenEstudo() {
             painter = painterResource(id = R.drawable.intro_logo),
             contentDescription = null,
             modifier = Modifier
+                .padding(top = 16.dp)
                 .background(Color.White)
                 .fillMaxWidth(),
             contentScale = ContentScale.Fit
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            text = stringResource(id = R.string.intro_title),
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(id = R.color.black),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text(
+            modifier = Modifier.padding(top = 16.dp),
+            text = stringResource(id = R.string.intro_sub_title),
+            fontSize = 16.sp,
+            color = colorResource(id = R.color.dark_gray),
+            textAlign = TextAlign.Center,
+            lineHeight = 24.sp
+        )
+
+        Button(
+            onClick = { onClick() },
+            modifier = Modifier
+                .padding(horizontal = 32.dp, vertical = 16.dp)
+                .fillMaxWidth()
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.purple),
+                contentColor = colorResource(id = R.color.white)
+            ),
+            shape = RoundedCornerShape(10.dp)
+
+        ) {
+            Text(
+                text = stringResource(id = R.string.let_go),
+                fontSize = 16.sp,
+                color = colorResource(id = R.color.white),
+                textAlign = TextAlign.Center,
+                lineHeight = 24.sp
+
+            )
+
+        }
+
+        Text(
+            modifier = Modifier.padding(top = 16.dp),
+            text = stringResource(id = R.string.alread_account),
+            fontSize = 16.sp,
+            color = colorResource(id = R.color.black),
+            textAlign = TextAlign.Center,
+            lineHeight = 24.sp
+
         )
 
     }
--- app/src/main/java/net/developermaster/projectbase/ui/theme/MainActivity.kt
+++ app/src/main/java/net/developermaster/projectbase/ui/theme/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import net.developermaster.projectbase.navigation.NavigationNavController
+import net.developermaster.projectbase.screens.UiScreenEstudo
 
 class MainActivity : ComponentActivity() {
 
@@ -29,7 +30,9 @@ class MainActivity : ComponentActivity() {
 
                 MaterialTheme {
 
-                    NavigationNavController()
+//                    NavigationNavController()
+
+                    UiScreenEstudo(onClick = {})
                 }
             }
         }
